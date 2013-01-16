2c366c599f564dafd70e0357fc4db24c75829fff
Mat Ryer <matryer@mac>
2013-01-16 08:34:03 -0700

    added MD5 as an option to signature

--- hash_func.go
+++ hash_func.go
@@ -1,19 +1,41 @@
 package signature
 
 import (
+	"crypto/md5"
 	"crypto/sha1"
 	"fmt"
+	"io"
 )
 
 // HashFunc represents funcs that can hash a string.
 type HashFunc func(s string) string
 
 // Hash hashes a string using the current HashFunc.
+//
+// To tell Signature to use a different hashing algorithm, you
+// just need to assign a different HashFunc to the Hash variable.
+//
+// To use the MD5 hash:
+//
+//     signature.Hash = signature.MD5Hash
+//
+// Or you can write your own hashing function:
+//
+//     signature.Hash = func(s string) string {
+//	     // TODO: do your own hashing here
+//     }
 var Hash HashFunc = SHA1Hash
 
-// SHA1Hash hashes a string using the SHA-1 algorithm.
+// SHA1Hash hashes a string using the SHA-1 hash algorithm as defined in RFC 3174.
 var SHA1Hash HashFunc = func(s string) string {
 	hash := sha1.New()
 	hash.Write([]byte(s))
 	return fmt.Sprintf("%x", hash.Sum(nil))
+}
+
+// MD5Hash hashes a string using the MD5 hash algorithm as defined in RFC 1321.
+var MD5Hash HashFunc = func(s string) string {
+	md5 := md5.New()
+	io.WriteString(md5, s)
+	return fmt.Sprintf("%x", md5.Sum(nil))
 }
--- hash_func_test.go
+++ hash_func_test.go
@@ -14,3 +14,9 @@ func TestSHA1Hash(t *testing.T) {
 	assert.Equal(t, SHA1Hash("abc"), "a9993e364706816aba3e25717850c26c9cd0d89d")
 
 }
+
+func TestMD5Hash(t *testing.T) {
+
+	assert.Equal(t, MD5Hash("abc"), "900150983cd24fb0d6963f7d28e17f72")
+
+}
