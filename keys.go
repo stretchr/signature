88f58476e2790ca040bfd1071f06f9b97a9ff3f6
Mat Ryer <matryer@mac.com>
2013-01-20 13:30:57 -0700

    improved random key generators

--- keys.go
+++ keys.go
@@ -1,31 +1,41 @@
 package signature
 
 import (
-	"bytes"
 	"math/rand"
 	"time"
 )
 
+// RandomKeyCharacters is a []byte of the characters to choose from when generating
+// random keys.
+var RandomKeyCharacters []byte = []byte("abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+// randomised gets whether the rand.Seed has been set or not.
+var randomised bool
+
+// RandomKey generates a random key at the given length.
+//
+// The first time this is called, the rand.Seed will be set
+// to the current time.
 func RandomKey(length int) string {
 
-	return randomString(length)
-
-}
-
-func randomString(l int) string {
-	var result bytes.Buffer
-	var temp string
-	for i := 0; i < l; {
-		if string(randInt(65, 90)) != temp {
-			temp = string(randInt(65, 90))
-			result.WriteString(temp)
-			i++
-		}
+	// randomise the seed
+	if !randomised {
+		rand.Seed(time.Now().UTC().UnixNano())
+		randomised = true
 	}
-	return result.String()
+
+	// Credit: http://stackoverflow.com/questions/12321133/golang-random-number-generator-how-to-seed-properly
+
+	bytes := make([]byte, length)
+	for i := 0; i < length; i++ {
+		randInt := randInt(0, len(RandomKeyCharacters))
+		bytes[i] = RandomKeyCharacters[randInt : randInt+1][0]
+	}
+	return string(bytes)
+
 }
 
+// randInt generates a random integer between min and max.
 func randInt(min int, max int) int {
-	rand.Seed(time.Now().UTC().UnixNano())
 	return min + rand.Intn(max-min)
 }
