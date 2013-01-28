d6e75c2721b11f2017326853a829503ecea68146
Mat Ryer <matryer@mac.com>
2013-01-27 21:29:31 -0700

    added HashWithKeys method to assist with Response signing

--- hash_func_test.go
+++ hash_func_test.go
@@ -20,3 +20,12 @@ func TestMD5Hash(t *testing.T) {
 	assert.Equal(t, MD5Hash("abc"), "900150983cd24fb0d6963f7d28e17f72")
 
 }
+
+func TestHashWithPrivateKey(t *testing.T) {
+
+	assert.Equal(t, HashWithKeys([]byte("some bytes to hash"), []byte("public key"), []byte("private key")), "f09b5012ffea295d3c199c1db9beaeaff3755501")
+	assert.NotEmpty(t, HashWithKeys([]byte("some bytes to hash"), []byte("public key"), []byte("private key")), HashWithKeys([]byte("some bytes to hash"), []byte("public key"), []byte("different private key")))
+	assert.NotEmpty(t, HashWithKeys([]byte("some bytes to hash"), []byte("public key"), []byte("private key")), HashWithKeys([]byte("some bytes to hash"), []byte("different public key"), []byte("private key")))
+	assert.NotEmpty(t, HashWithKeys([]byte("some bytes to hash"), []byte("public key"), []byte("private key")), HashWithKeys([]byte("different bytes to hash"), []byte("public key"), []byte("private key")))
+
+}
