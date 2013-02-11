2539ec5bda19b4ddb8b87e61607be2d37e1989f8
Mat Ryer <matryer@mac.com>
2013-02-11 15:28:50 -0700

    Added tracing abilities to Signature

---
+++ tracing.go
@@ -0,0 +1,9 @@
+package signature
+
+import (
+	"github.com/stretchrcom/tracer"
+)
+
+// Tracer (by default nil) is the object that the signature package
+// should trace to.
+var Tracer *tracer.Tracer = nil
